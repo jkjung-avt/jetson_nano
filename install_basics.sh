8c4536a4baace33bc452c0fb9507ca7f08bd3bec
JK Jung <jkjung13@gmail.com>
2019-05-14 20:31:26 +0800

    Add script to check and add CUDA stuffs into ~/.bashrc

---
+++ install_basics.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+if ! grep 'cuda/bin' ${HOME}/.bashrc > /dev/null ; then
+  echo "** Add CUDA stuffs into ~/.bashrc"
+  echo >> ${HOME}/.bashrc
+  echo "export PATH=/usr/local/cuda/bin:\${PATH}" >> ${HOME}/.bashrc
+  echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:\${LD_LIBRARY_PATH}" >> ${HOME}/.bashrc
+fi
+
