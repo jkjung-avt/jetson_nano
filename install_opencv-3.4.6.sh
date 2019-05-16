8cd05e1dab738c53f1368eedbcb926220faae95e
JK Jung <jkjung13@gmail.com>
2019-05-16 17:09:07 +0800

    Remove unnecessary username and password

--- install_opencv-3.4.6.sh
+++ install_opencv-3.4.6.sh
@@ -19,9 +19,6 @@
 
 set -e
 
-user="nvidia"
-passwd="nvidia"
-
 folder=${HOME}/src
 mkdir -p $folder
 
@@ -71,6 +68,7 @@ cd build/
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_CUDA=ON -D CUDA_ARCH_BIN="5.3" -D CUDA_ARCH_PTX="" -D WITH_CUBLAS=ON -D ENABLE_FAST_MATH=ON -D CUDA_FAST_MATH=ON -D ENABLE_NEON=ON -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make -j3
 sudo make install
+sudo ldconfig
 
 python3 -c 'import cv2; print("python3 cv2 version: %s" % cv2.__version__)'
 python2 -c 'import cv2; print("python2 cv2 version: %s" % cv2.__version__)'
