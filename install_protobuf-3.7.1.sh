1c6e2b2d5a3dc546a3d93299f5c281060911c030
JK Jung <jkjung13@gmail.com>
2019-05-20 20:05:36 +0800

    Fix a typo and add checking for existence of downloaded files

--- install_protobuf-3.7.1.sh
+++ install_protobuf-3.7.1.sh
@@ -7,8 +7,12 @@ mkdir -p $folder
 
 echo "** Download protobuf-3.7.1 sources"
 cd $folder
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protobuf-python-3.7.1.zip
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-aarch_64.zip
+if [ ! -f protobuf-python-3.7.1.zip ]; then
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protobuf-python-3.7.1.zip
+fi
+if [ ! -f protoc-3.7.1-linux-aarch_64.zip ]; then
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-aarch_64.zip
+fi
 
 echo "** Install protoc"
 unzip protobuf-python-3.7.1.zip
@@ -17,7 +21,7 @@ sudo cp protoc-3.7.1/bin/protoc /usr/local/bin/protoc
 
 echo "** Build and install protobuf-3.7.1 libraries"
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
-cd protobuf-3.6.1/
+cd protobuf-3.7.1/
 ./autogen.sh
 ./configure --prefix=/usr/local
 make
