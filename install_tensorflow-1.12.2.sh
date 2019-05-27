034ababde94e4d762080f5e55a94d098c7e87573
JK Jung <jkjung13@gmail.com>
2019-05-27 20:03:19 +0800

    Fix the error of --local_resources for the 'bazel build' command

--- install_tensorflow-1.12.2.sh
+++ install_tensorflow-1.12.2.sh
@@ -64,8 +64,7 @@ TF_SET_ANDROID_WORKSPACE=0 \
     ./configure
 bazel build --config=opt \
 	    --config=cuda \
-	    --local_ram_resources HOST_RAM*0.6 \
-	    --local_cpu_resources HOST_CPU*0.5 \
+	    --local_resources=2560.0,2.0,1.0 \
             //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package wheel/tensorflow_pkg
 
