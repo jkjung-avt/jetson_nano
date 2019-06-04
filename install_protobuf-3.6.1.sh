d814331666ec1f8bcb4f00131d64209c69269349
JK Jung <jkjung13@gmail.com>
2019-06-04 09:47:11 +0800

    Use protobuf 3.6.1 instead of 3.6.0

--- install_protobuf-3.6.1.sh
+++ install_protobuf-3.6.1.sh
@@ -5,23 +5,23 @@ set -e
 folder=${HOME}/src
 mkdir -p $folder
 
-echo "** Download protobuf-3.6.0 sources"
+echo "** Download protobuf-3.6.1 sources"
 cd $folder
-if [ ! -f protobuf-python-3.6.0.zip ]; then
-  wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.0/protobuf-python-3.6.0.zip
+if [ ! -f protobuf-python-3.6.1.zip ]; then
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-python-3.6.1.zip
 fi
-if [ ! -f protoc-3.6.0-linux-aarch_64.zip ]; then
-  wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.0/protoc-3.6.0-linux-aarch_64.zip
+if [ ! -f protoc-3.6.1-linux-aarch_64.zip ]; then
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-aarch_64.zip
 fi
 
 echo "** Install protoc"
-unzip protobuf-python-3.6.0.zip
-unzip protoc-3.6.0-linux-aarch_64.zip -d protoc-3.6.0
-sudo cp protoc-3.6.0/bin/protoc /usr/local/bin/protoc
+unzip protobuf-python-3.6.1.zip
+unzip protoc-3.6.1-linux-aarch_64.zip -d protoc-3.6.1
+sudo cp protoc-3.6.1/bin/protoc /usr/local/bin/protoc
 
-echo "** Build and install protobuf-3.6.0 libraries"
+echo "** Build and install protobuf-3.6.1 libraries"
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
-cd protobuf-3.6.0/
+cd protobuf-3.6.1/
 ./autogen.sh
 ./configure --prefix=/usr/local
 make
@@ -40,4 +40,4 @@ python3 setup.py build --cpp_implementation
 python3 setup.py test --cpp_implementation
 sudo python3 setup.py install --cpp_implementation
 
-echo "** Build protobuf-3.6.0 successfully"
+echo "** Build protobuf-3.6.1 successfully"
