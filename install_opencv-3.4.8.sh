605ba14f51c491a744d02bb4f1938650037c466f
JK Jung <jkjung13@gmail.com>
2019-11-26 12:39:32 +0800

    Remove python2 stuffs

--- install_opencv-3.4.8.sh
+++ install_opencv-3.4.8.sh
@@ -86,12 +86,19 @@ echo "** Building opencv..."
 mkdir build
 cd build/
 
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_CUDA=ON -D CUDA_ARCH_BIN=${cuda_compute} -D CUDA_ARCH_PTX="" -D WITH_CUBLAS=ON -D ENABLE_FAST_MATH=ON -D CUDA_FAST_MATH=ON -D ENABLE_NEON=ON -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D WITH_QT=ON -D WITH_OPENGL=ON ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D WITH_CUDA=ON -D CUDA_ARCH_BIN=${cuda_compute} -D CUDA_ARCH_PTX="" \
+      -D WITH_CUBLAS=ON -D ENABLE_FAST_MATH=ON -D CUDA_FAST_MATH=ON \
+      -D ENABLE_NEON=ON -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON \
+      -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
+      -D BUILD_opencv_python2=OFF -D BUILD_opencv_python3=ON \
+      -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF \
+      -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make -j3
 sudo make install
 sudo ldconfig
 
 python3 -c 'import cv2; print("python3 cv2 version: %s" % cv2.__version__)'
-python2 -c 'import cv2; print("python2 cv2 version: %s" % cv2.__version__)'
+#python2 -c 'import cv2; print("python2 cv2 version: %s" % cv2.__version__)'
 
 echo "** Install opencv-3.4.8 successfully"
