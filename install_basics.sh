908a762fae16e0ae190a35198a93fee7e4ed8c76
JK Jung <jkjung13@gmail.com>
2020-07-13 16:41:53 +0800

    Avoid redundant colons in PATH and LD_LIBRARY_PATH environment variables

--- install_basics.sh
+++ install_basics.sh
@@ -5,7 +5,7 @@ set -e
 if ! grep 'cuda/bin' ${HOME}/.bashrc > /dev/null ; then
   echo "** Add CUDA stuffs into ~/.bashrc"
   echo >> ${HOME}/.bashrc
-  echo "export PATH=/usr/local/cuda/bin:\${PATH}" >> ${HOME}/.bashrc
-  echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:\${LD_LIBRARY_PATH}" >> ${HOME}/.bashrc
+  echo "export PATH=/usr/local/cuda/bin\${PATH:+:\${PATH}}" >> ${HOME}/.bashrc
+  echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}" >> ${HOME}/.bashrc
 fi
 
