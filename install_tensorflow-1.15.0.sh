ca4fe35373b02c36791a2deb14948fdda992eb58
JK Jung <jkjung13@gmail.com>
2021-11-28 09:51:22 +0800

    Update pip3 install's according to issue #23

--- install_tensorflow-1.15.0.sh
+++ install_tensorflow-1.15.0.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+version=1.15.0
 script_path=$(realpath $0)
-patch_path=$(dirname $script_path)/tensorflow/tensorflow-1.15.0.patch
+patch_path=$(dirname $script_path)/tensorflow/tensorflow-${version}.patch
 trt_version=$(echo /usr/lib/aarch64-linux-gnu/libnvinfer.so.? | cut -d '.' -f 3)
 
 chip_id=$(cat /sys/module/tegra_fuse/parameters/tegra_chip_id)
@@ -41,23 +42,29 @@ fi
 
 echo "** Install requirements"
 sudo apt-get install -y libhdf5-serial-dev hdf5-tools
-sudo pip3 install -U pip six numpy wheel setuptools mock h5py
-sudo pip3 install -U keras_applications
-sudo pip3 install -U keras_preprocessing
+sudo pip3 install -U pip six wheel setuptools mock
+sudo pip3 install -U 'numpy<1.19.0'
+sudo pip3 install -U h5py==2.10.0
+sudo pip3 install pandas
+sudo pip3 install future
+sudo pip3 install -U keras_applications==1.0.8 --no-deps
+sudo pip3 install -U keras_preprocessing==1.1.2 --no-deps
 
 export LD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib64:$LD_LIBRARY_PATH
 
-echo "** Download and patch tensorflow-1.15.0"
+echo "** Download and patch tensorflow-${version}"
 pushd $folder
-if [ ! -f tensorflow-1.15.0.tar.gz ]; then
-  wget https://github.com/tensorflow/tensorflow/archive/v1.15.0.tar.gz -O tensorflow-1.15.0.tar.gz
+if [ ! -f tensorflow-${version}.tar.gz ]; then
+  wget https://github.com/tensorflow/tensorflow/archive/v${version}.tar.gz \
+       -O tensorflow-${version}.tar.gz
 fi
-tar xzvf tensorflow-1.15.0.tar.gz
-cd tensorflow-1.15.0
+tar xzvf tensorflow-${version}.tar.gz
+cd tensorflow-${version}
 
-patch -N -p1 < $patch_path && echo "tensorflow-1.15.0 source tree appears to be patched already.  Continue..."
+patch -N -p1 < $patch_path && \
+    echo "tensorflow-${version} source tree appears to be patched already.  Continue..."
 
-echo "** Configure and build tensorflow-1.15.0"
+echo "** Configure and build tensorflow-${version}"
 export TMP=/tmp
 PYTHON_BIN_PATH=$(which python3) \
 PYTHON_LIB_PATH=$(python3 -c 'import site; print(site.getsitepackages()[0])') \
@@ -82,17 +89,18 @@ GCC_HOST_COMPILER_PATH=$(which gcc) \
 CC_OPT_FLAGS="-march=native" \
 TF_SET_ANDROID_WORKSPACE=0 \
     ./configure
-bazel build --config=opt \
-	    --config=cuda \
-	    --local_resources=${local_resources} \
-            //tensorflow/tools/pip_package:build_pip_package
+bazel build \
+    --config=opt \
+    --config=cuda \
+    --local_resources=${local_resources} \
+    //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package wheel/tensorflow_pkg
 
-echo "** Install tensorflow-1.15.0"
-#sudo pip3 install wheel/tensorflow_pkg/tensorflow-1.15.0-cp36-cp36m-linux_aarch64.whl
-sudo pip3 install wheel/tensorflow_pkg/tensorflow-1.15.0-*.whl
+echo "** Install tensorflow-${version}"
+#sudo pip3 install wheel/tensorflow_pkg/tensorflow-${version}-cp36-cp36m-linux_aarch64.whl
+sudo pip3 install wheel/tensorflow_pkg/tensorflow-${version}-*.whl
 
 popd
 python3 -c "import tensorflow as tf; print('tensorflow version: %s' % tf.__version__)"
 
-echo "** Build and install tensorflow-1.15.0 successfully"
+echo "** Build and install tensorflow-${version} successfully"
